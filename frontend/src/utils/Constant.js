7a5246a6bb5c259b091e9d7b53f9f0b3780bd21f
Ashish Kumar Yadav <103557142+Ashish32507@users.noreply.github.com>
2025-05-16 15:25:09 +0530

    Update Constant.js

--- frontend/src/utils/Constant.js
+++ frontend/src/utils/Constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "http://localhost:3000/user";
-export const PRODUCT_API_END_POINT = "http://localhost:3000/product";
+export const USER_API_END_POINT = "https://deploy-jigq.onrender.com/user";
+export const PRODUCT_API_END_POINT = "https://deploy-jigq.onrender.com/product";
